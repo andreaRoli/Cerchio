8ef8232e0e0001c2c9287e93af7c4a7f46410227
andreaRoli <aggiustatuttobob4@gmail.com>
2024-01-31 17:54:22 +0100

    Aggiungere i file di progetto.

---
+++ Cerchio/Cerchio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Cerchio
+    {
+        public double Raggio { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}", Raggio);
+        }
+
+        public static Cerchio Parse(string s)
+        {
+            Cerchio cerchio = new Cerchio();
+            cerchio.Raggio = int.Parse(s);
+            return cerchio;
+        }
+    }
+}
---
+++ Cerchio/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cerchio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio n = new Cerchio();
+            Console.WriteLine("Quanto vale il raggio?");
+            n.Raggio=double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Il raggio del cherchio = {0}", n.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
